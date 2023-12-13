67b8883e11d9f6f867141b3fe2ffcde4f32bf3e5
LuizPC <gustavo80moura@gmail.com>
2023-12-13 20:53:06 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { SignIn } from './sreens/SignIn/intex'
+
+export default function App() {
+  return <SignIn />
+}
