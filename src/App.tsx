b2f812e0d5d53b5040743820124f4c56ddfff31c
LuizPC <gustavo80moura@gmail.com>
2023-12-13 21:00:05 -0300

    Package: Styled-components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components'
 import { SignIn } from './sreens/SignIn/intex'
+import theme from './theme'
 
 export default function App() {
-  return <SignIn />
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  )
 }
